28eac39079b767b23d810acea2d0de8a988ac629
YgorillaKim <00ghks22@naver.com>
2019-11-20 14:57:57 +0000

    django 관리자 페이지 생성 pip install bootstrap-admin ChangWon 폴더에 메인 templates 생성

--- ChangWon/mainsite/admin.py
+++ ChangWon/mainsite/admin.py
@@ -6,6 +6,10 @@ from .models import Contact
 from .models import Portfolio
 
 # Register your models here.
+
+# 2019.11.20 김영환 수정 
+admin.site.site_header = '관리자 페이지입니다.' # 관리자 페이지 Header입니다.
+
 admin.site.register(Intro)
 admin.site.register(About)
 admin.site.register(Service)
